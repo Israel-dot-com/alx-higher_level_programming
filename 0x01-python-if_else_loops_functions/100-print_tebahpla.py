3fd30e2e1f312b6a47a42e83518c6f64fbfdebf5
Israel <isltimi1000@gmail.com>
2022-08-24 10:01:43 +0000

    Use format string

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(-122, -96):
-    # print(f"{chr(i)}", end="")
-    print(f"{chr(i*(-1))}", end="")
+    #print(f"{chr(i*(-1))}", end="")
+    print("{0}".format(chr(i*(-1))), end="")
