76dc1b2f33d375968ffa2a1107ac901611ec68a7
Israel <isltimi1000@gmail.com>
2022-08-24 04:11:07 +0000

    Add proper indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 numberx = abs(number) % 10
 if number < 0:
-	numberx = -numberx
+   numberx = -numberx
 print(f"Last digit of {number:d} is {numberx} and is ", end='')
 if numberx == 0:
-	print(f"0")
+   print(f"0")
 elif numberx > 5:
-	print(f"greater than 5")
+   print(f"greater than 5")
 else:
-	print(f"less than 6 and not 0")
+   print(f"less than 6 and not 0")
