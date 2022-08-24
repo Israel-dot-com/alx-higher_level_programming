e99d096971c5389cbae1064f988657b2a8884bff
Israel <isltimi1000@gmail.com>
2022-08-24 10:42:05 +0000

    Added conditional

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(-122, -96):
-    #print(f"{chr(i*(-1))}", end="")
-    print("{0}".format(chr(i*(-1))), end="")
+    if i % 2 == 0:
+        print("{0}".format(chr(i*(-1))).lower(), end="")
+    else:
+        print("{0}".format(chr(i*(-1))).upper(), end="")
