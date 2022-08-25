f209b3c453f50ca493fa05f5cd5e46a225f861e3
Israel <isltimi1000@gmail.com>
2022-08-25 18:22:01 +0000

    Add conditional

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 1
-addition = add(a, b)
-print("{0} + {1} = {2}".format(a, b, addition))
+if __name == "__main__"
+    from add_0 import add
+    a = 1
+    b = 1
+    addition = add(a, b)
+    print("{0} + {1} = {2}".format(a, b, addition))
