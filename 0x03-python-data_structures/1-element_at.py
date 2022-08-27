b0a376fa7a933de8406eece592b9dadecbbd5d61
Israel <isltimi1000@gmail.com>
2022-08-27 17:49:15 +0000

    Add return in Retrieve task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,4 +6,4 @@ def element_at(my_list, idx):
     elif idx >= leng:
         return None
     else:
-        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
+        return my_list[idx]
