cf2b3aad309739a2431ac878ca19b20915bd2e91
Israel <isltimi1000@gmail.com>
2022-08-29 10:37:48 +0000

    Add correction to copy task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     maxindex = len(my_list) - 1
     new_list = my_list.copy()
     if idx < 0 or idx > maxindex:
-        return new_list
+        return my_list.copy( )
     else:
-        my_list[idx] = element
-        return my_list
+        new_list[idx] = element
+        return new_list
