4eff57ee2fe33d519dcf5fbe26abd02e6e7159ab
Israel <isltimi1000@gmail.com>
2022-08-29 11:33:28 +0000

    Add no C task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    listitem = list(my_string)
-    countitem = listitem.count('c') - 1
-    i = 0
-    while i < countitem:
-        if 'c' in listitem:
-            listitem.remove('c')
-        else:
-            break
-        i += 1
-return listitem
+    new_str = my_string.translate({ord(i): None for i in 'Cc'})
+    return new_str
