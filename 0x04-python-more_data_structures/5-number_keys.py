2d8c6681024f607e64dc45a60ccd404b21072f01
Israel <isltimi1000@gmail.com>
2022-09-04 16:12:16 +0000

    Fixed return with shorter code

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    keycounter = 0
-    listofkeys = (list(a_dictionary.keys())
-
-    for i in listofkeys:
-        keycounter += 1
-                  
-    return keycounter
+    return len(a_dictionary.keys())
