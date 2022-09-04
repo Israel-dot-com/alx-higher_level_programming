b688af230181ea1a45046fe03f9ad14cb6c13601
Israel <isltimi1000@gmail.com>
2022-09-04 16:10:20 +0000

    Fixed return

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,7 +2,8 @@
 def number_keys(a_dictionary):
     keycounter = 0
     listofkeys = (list(a_dictionary.keys())
+
     for i in listofkeys:
         keycounter += 1
                   
-    return (keycounter)
+    return keycounter
