81c3e3ca6240404a4301025a4cdff3fbf2249de1
Israel <isltimi1000@gmail.com>
2022-09-14 04:40:16 +0000

    Add Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
-"""Define an empty Square class."""
+"""Define a Square class."""
 
 
 class Square:
-    """Empty Square class"""
-    pass
+    """ A square"""
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size 
