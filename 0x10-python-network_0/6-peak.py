623b9f8992d6907f146fb08fd243a6087fbaf846
Israel <isltimi1000@gmail.com>
2022-12-30 20:21:57 +0000

    Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""Finds the max in a list of unsorted integers"""
+"""Finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds max in list_of_integers list"""
+    """Finds a peak in list_of_integers"""
 
     if list_of_integers is None or list_of_integers == []:
         return None
-    low_index = 0
-    high_index = len(list_of_integers)
-    mid_index = ((high_index - low_index) // 2) + low_index
-    mid_index = int(mid_index)
-    if high_index == 1:
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
         return list_of_integers[0]
-    if high_index == 2:
+    if hi == 2:
         return max(list_of_integers)
-    if list_of_integers[mid_index] >= list_of_integers[mid_index - 1] and\
-            list_of_integers[mid_index] >= list_of_integers[mid_index + 1]:
-        return list_of_integers[mid_index]
-    if mid_index > 0 and list_of_integers[mid_index] < \
-        list_of_integers[mid_index + 1]:
-        return find_peak(list_of_integers[mid_index:])
-    if mid_index > 0 and list_of_integers[mid_index] < \
-        list_of_integers[mid_index - 1]:
-        return find_peak(list_of_integers[:mid_index])
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
